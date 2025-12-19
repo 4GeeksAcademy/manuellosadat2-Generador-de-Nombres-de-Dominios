8e3e6d4d29072df58979fa52bc7438ea3c6eab0c
manuellosadat2-a11y <manuellosadat2@gmail.com>
2025-12-19 03:13:24 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
+let extensions = ['.com', '.net', '.io'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for(let a = 0; a < pronoun.length; a++){
+    for(let b = 0; b < adj.length; b++){
+        for(let c = 0; c < noun.length; c++){
+            for(let d = 0; d < extensions.length; d++){
+                console.log(`${pronoun[a]}${adj[b]}${noun[c]}${extensions[d]}`)
+            }
+        }
+    }
+}
